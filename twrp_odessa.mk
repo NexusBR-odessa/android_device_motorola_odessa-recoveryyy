1889e142670eaf9f7d09d8dbfb1964e3280ecc7e
NexusBR-odessa <miguel03barreto@gmail.com>
2025-07-22 15:16:29 -0300

    deus, eu preciso de um milagre v8 (camaro exorcist)

--- twrp_odessa.mk
+++ twrp_odessa.mk
@@ -17,8 +17,20 @@
 # Release name
 PRODUCT_RELEASE_NAME := odessa
 
-# Inherit from our custom product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Inherit from avicii device
+$(call inherit-product, device/motorola/odessa/device.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := odessa
